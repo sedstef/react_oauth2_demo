7b51cd882de0355a5d8beb5e535cebee0173d9df
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-03-27 18:30:36 +0100

    Add simple greeting call

--- client_oauth2_google/src/App.js
+++ client_oauth2_google/src/App.js
@@ -1,19 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState();
+
+  function fetchGreeting(){
+      //const API_BASEURL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : '';
+    const API_BASEURL = 'localhost:8080'
+        fetch(`${API_BASEURL}/api/v1/hello`, {
+        method: 'GET',
+        headers: {
+          //Authorization: `Bearer ${user.access_token}`,
+          Accept: 'application/json'
+        }
+      })
+          .then(response => response.json())
+          .then(data => setGreeting(data.content))
+          .catch((error) => console.log('ERROR in get greeting: ' + error));
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={fetchGreeting}>Greeting</button>
+
+        <h1>{greeting}</h1>
+
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>
